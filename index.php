30d34769ef49705bafab34200972e359e7963cee
safnick <safnick@yandex.ru>
2021-08-18 18:57:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -106,18 +106,18 @@ $postings = [
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <?php foreach ($postings as $key => $value): ?>
+            <?php foreach ($postings as $post): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$value['img_url']; ?>" width="350" height="260" alt="">
+                    <img src="<?=$post['img_url']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$value['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['name']; ?></a></h3>
+                    <span class="lot__category"><?=$post['category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$post['name']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$value['price']; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$post['price']; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
